2bb50f4c77868f2c89e46787d1e229a919fa600a
boycany <yurayurateiko@gmail.com>
2022-08-23 10:28:31 +0800

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: ['./src/App.js', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, './build'),
+        // path.resolve 將相對路徑轉為絕對路徑
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+            inject: true,
+            minify: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: './css/index.css'
+        })
+    ],
+    devServer: {
+        port: 3000,
+    }
+}
